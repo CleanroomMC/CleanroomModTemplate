ab92bf0bfd3c2ada4e040d33abb4c280fb732cd6
arcade_kappa <gkappa@outlands.top>
2025-04-04 17:47:51 +0800

    Basic scala setup

---
+++ src/main/scala/com/example/modid/Reference.scala
@@ -0,0 +1,12 @@
+package com.example.modid;
+
+/**
+ * Tags storage class, you can change at will
+ */
+object Reference {
+
+    final val MOD_ID = "modid"
+    final val MOD_NAME = "Mod Name"
+    final val VERSION = "1.0.0"
+
+}
